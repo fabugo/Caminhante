a729cd841c50799b572af5e6de921f1aa51156d4
HenriqueB13 <henriquepardo@gmail.com>
2019-02-12 14:31:23 -0200

    Update robo.c

--- Solucao/robo.c
+++ Solucao/robo.c
@@ -14,23 +14,88 @@ void checaCon()
   nxtDisplayCenteredTextLine(4, "Conectado");
   wait1Msec(3000);
 }
+void moveForward(int centimeters, int velocity) {
+  int goal = 20.473 * centimeters;
+
+  nMotorEncoder[motorA] = 0;
+  nMotorEncoder[motorB] = 0;
+
+  nMotorEncoderTarget[motorA] = goal;
+
+  while(nMotorEncoder[motorA] < goal) {
+    motor[motorA] = velocity;
+    motor[motorB] = velocity;
+  }
+  motor[motorA] = 0;
+  motor[motorB] = 0;
+}
+
+void rotateToRight(int velocity, int degrees) {
+  //20.473 * 0.148 = 3.03
+  int goal = 3 * degrees;
+
+  nMotorEncoder[motorA] = 0;
+  nMotorEncoder[motorB] = 0;
+
+  nMotorEncoderTarget[motorA] = goal;
+
+  while(nMotorEncoder[motorA] < goal) {
+    motor[motorA] = velocity;
+    motor[motorB] = -velocity;
+  }
+  motor[motorA] = 0;
+  motor[motorB] = 0;
+}
+void rotateToLeft(int velocity, int degrees) {
+  //20.473 * 0.148 = 3.03
+  int goal = 3 * degrees;
+
+  nMotorEncoder[motorA] = 0;
+  nMotorEncoder[motorB] = 0;
+
+  nMotorEncoderTarget[motorB] = goal;
+
+  while(nMotorEncoder[motorB] < goal) {
+    motor[motorA] = -velocity;
+    motor[motorB] = velocity;
+  }
+  motor[motorA] = 0;
+  motor[motorB] = 0;
+}
 void readDataMsg(){
   int messageTam;
+  int valor;
+  int aux2 =0;
+  char auxiliar[4];
+  int j=0;
+  int i = 0;
   messageTam = cCmdMessageGetSize(entrada);
   if(messageTam<=0)
     return;
   char bufferEntrada[100];
   cCmdMessageRead(bufferEntrada, messageTam, entrada);
 
-  for(int i = 0; i < messageTam; i++){
-    if(i%2!=0){
-     if (bufferEntrada[i]<0){
-       rotateToLeft(velocity, bufferEntrada[i]);
-      }else
-           rotateToRight(velocity, bufferEntrada[i]);
-    }
-    else{
-      moveForward(bufferEntrada[i], velocity);
+  for(i; i < messageTam; i++){
+    if(bufferEntrada[i]!=',' && aux2%2==0){
+      aux2++;
+      for(j=i; j<= i+3; j++){
+        auxiliar[j]= bufferEntrada[j];
+      }
+      i=i+3;
+      valor = atoi(auxiliar);
+
+      if (valor<0)
+        rotateToLeft(velocity, valor);
+      else
+        rotateToRight(velocity, valor);
+      }else if(bufferEntrada[i]!=',' && aux2%2!=0){
+      for(j=i; j<= i+3; j++){
+        auxiliar[j]= bufferEntrada[j];
+      }
+      aux2++;
+      i=i+3;
+      valor= atoi(auxiliar);
+      moveForward(valor, velocity);
     }
   }
   return;
